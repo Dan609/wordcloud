9aee86fa675035fd214014819e3c7f4fbf3b7d8e
Dan Bobkov <dan.bobkov@gmail.com>
2019-02-21 17:06:08 +0300

    .

--- pdf_scan_mac.R
+++ pdf_scan_mac.R
@@ -1,7 +1,26 @@
+#### text analysis
+
+options(max.print=100000)
+
+library(tm)  # for text mining
+library(SnowballC) # for text stemming
+library(wordcloud) # word-cloud generator
+library(RColorBrewer) # color palettes
+library(RDSTK)
+library(readr)
+library(twitteR)
+library(qdap)
+library(syuzhet)
+library(ggplot2)
+library(dplyr)
+library(pdftools)
+library(devtools)
+library(tcltk)
+
 ####### Directory scan Mac solution  #tkchooseDirectory(),
 
 file_list <- list.files(path = "/Users/danilabobkov/Dropbox/bks/bio/Age",
-                        pattern = "pdf", 
+                        pattern = "pdf",
                         all.files = FALSE,
                         full.names = TRUE, recursive = TRUE,
                         ignore.case = FALSE, include.dirs = FALSE,
@@ -9,12 +28,12 @@ file_list <- list.files(path = "/Users/danilabobkov/Dropbox/bks/bio/Age",
 file_list
 
 for (file_name in file_list) {
-  
+
   text <- pdf_text(file_name)
-  
+
   # Load the data as a corpus
   docs <- Corpus(VectorSource(text))
-  
+
   # Text cleaning :
   # Remove your own stop word, specify your stopwords as a character vector
   # docs <- tm_map(docs, removeWords, c("???"))
@@ -24,19 +43,17 @@ for (file_name in file_list) {
   docs <- tm_map(docs, removeWords, stopwords("russian"))
   docs <- tm_map(docs, removePunctuation)
   docs <- tm_map(docs, stripWhitespace)
-  
+
   # Build a term-document matrix
   dtm <- TermDocumentMatrix(docs)
   m <- as.matrix(dtm)
   v <- sort(rowSums(m),decreasing=TRUE)
   d <- data.frame(word = names(v),freq=v)
-  
+
   set.seed(1234)
-  wordcloud(words = d$word, freq = d$freq, 
+  wordcloud(words = d$word, freq = d$freq,
             scale=c(4,0.8),
-            min.freq = 1, max.words=100, 
-            random.order=FALSE, rot.per=0.1, 
+            min.freq = 1, max.words=100,
+            random.order=FALSE, rot.per=0.1,
             colors=brewer.pal(8, "Dark2"))
 }
-
-
